26b3086ce7be6b5b1a190ae22b82d91956e37459
ladoijo <ladoijo@gmail.com>
2017-05-05 10:43:18 +0700

    Initialize Project

---
+++ src/main/java/com/ladoijo/ws/ExpRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.ladoijo.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpRestApiApplication.class, args);
+	}
+}
